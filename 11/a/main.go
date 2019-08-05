24e4e457867d8a620f70c055b55f1b9beaaca734
Jeyko <jakeactually@outlook.com>
2019-08-05 14:21:19 -0500

    day 11

---
+++ 11/a/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strings"
+)
+
+func main() {
+	file, _ := ioutil.ReadFile("input.txt")
+	text := string(file)
+	jumps := strings.Split(text, ",")
+
+	var x, y, z int
+
+	for _, jump := range jumps {
+		switch jump {
+		case "n":
+			x--
+			y++
+		case "ne":
+			x--
+			z++
+		case "se":
+			y--
+			z++
+		case "s":
+			x++
+			y--
+		case "sw":
+			x++
+			z--
+		case "nw":
+			y++
+			z--
+		}
+	}
+
+	man := math.Abs(float64(x)) +
+		math.Abs(float64(y)) +
+		math.Abs(float64(z))
+
+	fmt.Println(man / 2)
+}

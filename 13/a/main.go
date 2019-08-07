19fd7fccc8cce7b5a725fcefd66635a7f060ba5f
Jeyko <jakeactually@outlook.com>
2019-08-07 16:02:48 -0500

    day 13 part 1

---
+++ 13/a/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+type Layer struct {
+	depth int
+	rng   int
+	pos   int
+	down  bool
+}
+
+func main() {
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	regex, _ := regexp.Compile("\\d+")
+	var layers []Layer
+
+	for scanner.Scan() {
+		strs := regex.FindAllString(scanner.Text(), -1)
+		depth, _ := strconv.Atoi(strs[0])
+		rng, _ := strconv.Atoi(strs[1])
+		layers = append(layers, Layer{depth, rng, 0, true})
+	}
+
+	sum := 0
+	for x := 0; x < 98+1; x++ {
+		for j := range layers {
+			layer := &layers[j]
+
+			if x == layer.depth && layer.pos == 0 {
+				sum += layer.depth * layer.rng
+			}
+
+			layer.step()
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func (layer *Layer) step() {
+	if layer.down {
+		layer.pos++
+		if layer.pos == layer.rng-1 {
+			layer.down = false
+		}
+	} else {
+		layer.pos--
+		if layer.pos == 0 {
+			layer.down = true
+		}
+	}
+}
---
+++ 13/b/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+
+	"gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/simple"
+	"gonum.org/v1/gonum/graph/topo"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	regex, _ := regexp.Compile("\\d+")
+	g := simple.NewUndirectedGraph()
+
+	for scanner.Scan() {
+		strs := regex.FindAllString(scanner.Text(), -1)
+
+		var nums []int64
+		for _, chr := range strs {
+			num, _ := strconv.Atoi(chr)
+			nums = append(nums, int64(num))
+		}
+
+		node := nodeOrNew(g, nums[0])
+		for _, num := range nums[1:] {
+			neighbor := nodeOrNew(g, num)
+			if node != neighbor {
+				g.SetEdge(g.NewEdge(node, neighbor))
+			}
+		}
+	}
+
+	fmt.Println(len(topo.ConnectedComponents(g)))
+}
+
+type Node struct {
+	id int64
+}
+
+func (node Node) ID() int64 {
+	return node.id
+}
+
+func nodeOrNew(g *simple.UndirectedGraph, id int64) graph.Node {
+	node := g.Node(id)
+	if node == nil {
+		newNode := Node{id}
+		g.AddNode(newNode)
+		return newNode
+	}
+	return node
+}
